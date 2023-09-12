309f1b13dc1a7f91032974a3f1327b6dad5645af
Denitsa Slavkova <deni9309@gmail.com>
2023-09-12 18:15:31 +0300

    Add project files.

---
+++ PaymentAPI/PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public Guid PaymentDetailId { get; set; }
+
+        [Required]
+        [Column(TypeName ="nvarchar(100)")]
+        public required string CardOwnerName { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(16)")]
+        public required string CardNumber { get; set; }
+        
+        [Required]
+        [Column(TypeName = "nvarchar(5)")]
+        public required string ExpirationDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(3)")]
+        public required string SecurityCode { get; set; }
+    }
+}
